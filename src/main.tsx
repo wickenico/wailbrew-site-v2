37cfbb4dd5d5c8664165a4d3dfe39e097a438258
Nico Wickersheim <nico.wickersheim@sevdesk.de>
2025-10-15 22:59:34 +0200

    Update `@vercel/analytics` import to use `next` package

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,5 @@
 import { createRoot } from "react-dom/client";
-import { Analytics } from "@vercel/analytics/react";
+import { Analytics } from "@vercel/analytics/next"
 import App from "./App.tsx";
 import "./index.css";
 
