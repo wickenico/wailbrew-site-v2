1cfae660845ae14f69a346540837fa7c5dca9de2
Nico Wickersheim <nico.wickersheim@sevdesk.de>
2025-10-17 12:52:35 +0200

    Add `@vercel/speed-insights` integration for performance monitoring

--- src/main.tsx
+++ src/main.tsx
@@ -2,10 +2,12 @@ import { createRoot } from "react-dom/client";
 import { Analytics } from "@vercel/analytics/react";
 import App from "./App.tsx";
 import "./index.css";
+import {SpeedInsights} from "@vercel/speed-insights/react";
 
 createRoot(document.getElementById("root")!).render(
   <>
     <App />
     <Analytics />
+      <SpeedInsights />
   </>
 );
