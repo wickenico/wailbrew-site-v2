092a3ad51ac919e51ce36b6710366859bdd9021f
Nico Wickersheim <nico.wickersheim@sevdesk.de>
2025-10-17 12:55:24 +0200

    Revert "Update `@vercel/speed-insights` import to use `next` package"
    This reverts commit 0aa9a9aaf1268c5b8592a2f4921a63bb701e96db.

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import { createRoot } from "react-dom/client";
 import { Analytics } from "@vercel/analytics/react";
 import App from "./App.tsx";
 import "./index.css";
-import {SpeedInsights} from "@vercel/speed-insights/next";
+import {SpeedInsights} from "@vercel/speed-insights/react";
 
 createRoot(document.getElementById("root")!).render(
   <>
